db3664d5fe25ef52cedc6bd7f3db76c825eaf4d2
lmadhuranga <lilan.maduranga@gmail.com>
2020-06-23 17:16:03 +0530

    update sh file

--- up.sh
+++ up.sh
@@ -1,14 +1,18 @@
 cd Order-Management-System-React.js--Frontend
 
+echo "**********************************************  Git reset "
 git reset --hard
 
+echo "**********************************************  Git get update "
 git pull origin master
 
+echo "**********************************************  Build the project"
 yarn build
 
+echo "**********************************************  Build Completed "
 echo "Build is completed "
 
+echo "**********************************************  Start file copying !"
 sudo cp -R build/* /var/www/html/
 
-echo "**********************************************"
-echo "File trafer completed to the server folder"
+echo "**********************************************  File trafer completed to the server folder"
